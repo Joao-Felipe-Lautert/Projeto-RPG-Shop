157383379e81661e42ae428b5742e447043af5df
JOAO FELIPE LAUTERT <joao.felipe.lautert@escola.pr.gov.br>
2026-03-12 10:45:33 +0000

    step 3

--- script.js
+++ script.js
@@ -38,14 +38,13 @@ function atualizarInterface() {
 
   // Cria o HTML de cada item (Loop)
   itens.forEach((item) => {
+    // Dentro do loop forEach...
     let div = document.createElement("div");
     div.className = "item-card";
 
     // ADICIONE ESTA LINHA ABAIXO:
     // Quando clicar, chama a função comprarItem enviando o ID deste produto
     div.onclick = () => comprarItem(item.id);
-
-    div.className = "item-card";
 
     if (item.estoque === 0) {
       div.style.opacity = "0.3"; // Deixa transparente
@@ -66,6 +65,15 @@ function atualizarInterface() {
   document.getElementById("hero-gold").innerText = heroi.ouro;
   document.querySelector(".hero-avatar").innerText = heroi.avatar;
   document.getElementById("hero-msg").innerText = heroi.fala;
+
+  let totalVendas = alasql("SELECT COUNT(*) AS qtd FROM vendas")[0].qtd;
+  document.getElementById("total-vendas").innerText = totalVendas;
+
+  let caixaTotal = alasql("SELECT SUM(valor) AS total FROM vendas")[0].total;
+  if (!caixaTotal) {
+    caixaTotal = 0;
+  }
+  document.getElementById("caixa-loja").innerText = caixaTotal;
 }
 
 // FUNÇÃO: Troca de Cliente (Lógica de Fila)
@@ -75,17 +83,8 @@ function proximoCliente() {
   atualizarInterface();
   alert("Cliente trocado!");
 }
-
-// Inicializa o jogo ao carregar a página
+//iniciar o jogo ao carregar a pagina
 window.onload = atualizarInterface;
-
-let div = document.createElement("div");
-div.className = "item-card";
-
-// ADICIONE ESTA LINHA ABAIXO:
-// Quando clicar, chama a função comprarItem enviando o ID deste produto
-div.onclick = () => comprarItem(item.id);
-
 // =========================================
 // LÓGICA DE VENDAS
 // =========================================
@@ -100,10 +99,13 @@ function comprarItem(idItem) {
     alert("Você não tem dinheiro!");
     return; // O 'return' para a função aqui e impede a compra
   }
+
+  //Buscar se contem estoque do iten
   if (item.estoque <= 0) {
-    alert("O estoque acabou, volte mais tarde");
-    return;
+    alert("Estamos sem estoque deste item, volte outra hora.");
+    return; // ira retornar somente se nao contiver mais estoque deste item
   }
+
   // 2. EXECUTAR A COMPRA (UPDATE)
   // Diminui 1 do estoque
   alasql(`UPDATE itens SET estoque = estoque - 1 WHERE id = ${idItem}`);
@@ -112,7 +114,15 @@ function comprarItem(idItem) {
   alasql(
     `UPDATE herois SET ouro = ouro - ${item.preco} WHERE id = ${heroiAtualId}`
   );
-
+  alasql(
+    "INSERT INTO vendas VALUES (" +
+      heroiAtualId +
+      ", " +
+      idItem +
+      ", " +
+      item.preco +
+      ")"
+  );
   // 3. FEEDBACK
   atualizarInterface(); // Redesenha a tela com os novos números
   logSQL(`Venda realizada: ${item.nome} por ${item.preco} moedas.`);
@@ -124,4 +134,63 @@ function logSQL(texto) {
   let linha = document.createElement("div");
   linha.innerText = `> ${texto}`;
   logDiv.prepend(linha);
+}
+// FUNÇÃO: Exporta os dados cruzados para um arquivo .txt
+function gerarRelatorio() {
+  // 1. O JOIN: Cruza as três tabelas
+  let relatorio = alasql(
+    "SELECT herois.nome AS cliente, itens.nome AS produto FROM vendas JOIN herois ON vendas.id_heroi = herois.id JOIN itens ON vendas.id_item = itens.id"
+  );
+
+  // 2. Monta o texto do arquivo
+  let texto = "::: RELATÓRIO OFICIAL DA GUILDA :::\n\n";
+  relatorio.forEach((linha) => {
+    texto += linha.cliente + " comprou " + linha.produto + "\n";
+  });
+
+  // 3. A Mágica do Download (Cria um arquivo na memória e baixa)
+  let arquivo = new Blob([texto], { type: "text/plain" });
+  let link = document.createElement("a");
+  link.href = URL.createObjectURL(arquivo);
+  link.download = "livro_caixa.txt";
+  link.click(); // Simula um clique no link
+}
+function fecharCaixa() {
+  // Zerar o caixa da loja
+  alasql("DELETE FROM vendas"); // Limpa todas as vendas
+  alert("Caixa fechado! Todas as vendas foram apagadas.");
+  atualizarInterface();
+}
+
+// FUNÇÃO: Gasta o dinheiro do caixa para subir o estoque
+function reabastecerEstoque() {
+  // 1. Descobre quanto dinheiro a loja tem (SELECT SUM)
+  let caixaAtual = alasql("SELECT SUM(valor) AS total FROM vendas")[0].total;
+  if (!caixaAtual) {
+    caixaAtual = 0;
+  }
+
+  // 2. Validação: Temos 100 moedas para pagar o fornecedor?
+  if (caixaAtual < 100) {
+    alert("O fornecedor cobra 100 moedas. Você está pobre!");
+    return;
+  }
+
+  // 3. A Transação de Compra (O Segredo do Fluxo de Caixa)
+  // Atualiza TODOS os itens de uma vez só somando 5 no estoque
+  alasql("UPDATE itens SET estoque = estoque + 5");
+
+  // Como registramos uma despesa no banco de dados? Usando um valor NEGATIVO!
+  // Usamos os IDs 0 e 0 pois não foi um herói nem um item específico
+  alasql("INSERT INTO vendas VALUES (0, 0, -100)");
+
+  // 4. Feedback Visual
+  logSQL("Despesa: Pagamento de Fornecedor (-100 moedas). Estoque renovado!");
+  atualizarInterface();
+}
+
+function InflaçãoMagica() {
+  // Aumenta o preço de todos os items em 10
+  alasql("UPDATE itens SET preco = preco + 10");
+  atualizarInterface();
 }
